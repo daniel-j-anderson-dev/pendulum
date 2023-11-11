750e0c672195017353987260ab6adfee97abffd5
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-11-10 23:57:22 -0500

    Simplified.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,26 @@
-use std::f32::consts::FRAC_PI_4;
-use macroquad::prelude::*;
+use std::f64::consts::FRAC_PI_4;
+use glam::DVec2;
 
 pub const GRAVITY: f64 = 9.8;
 pub const ITERATION_MAX: usize = 100;
-pub const THICKNESS: f32 = 1.0;
-pub const MASS_RADIUS: f32 = 10.0;
+pub const THICKNESS: f64 = 1.0;
+pub const MASS_RADIUS: f64 = 10.0;
 
-#[macroquad::main("pendulum")]
-async fn main() {
+fn main() {
+    let angles: Vec<f64> = Vec::with_capacity(ITERATION_MAX);
+
     // pendulum
+    let mut anglular_acceleration = 0.0;
     let mut angle = FRAC_PI_4;
     let length = 150.0;
-    let pivot = Vec2::new(screen_width(), screen_height()) * 0.5; // origin of screen
-    let mut mass_position = pivot + Vec2::new(angle.sin(), angle.cos()) * length;
+    let pivot = DVec2::new(0.0, 0.0) * 0.5; // origin of screen
+    let mut mass_position = pivot + DVec2::new(angle.sin(), angle.cos()) * length;
     
-    loop {
-        clear_background(LIGHTGRAY);
-
+    for time in 0..ITERATION_MAX {
         // TODO: fix me!!!
         angle += 0.02; 
 
         // update the mass position given the new angle
-        mass_position = pivot + Vec2::new(angle.sin(), angle.cos()) * length;
-
-        // draw pendulum
-        draw_line(
-            pivot.x, pivot.y,
-            mass_position.x, mass_position.y,
-            THICKNESS, BLACK
-        );
-        draw_circle(
-            mass_position.x, mass_position.y,
-            MASS_RADIUS, BLACK
-        );
-
-        next_frame().await;
+        mass_position = pivot + DVec2::new(angle.sin(), angle.cos()) * length;
     }
 }
